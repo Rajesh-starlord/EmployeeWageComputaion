cfa3d1b1b8a903452cbbb5bfe87e44c20aa6cd06
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-27 23:34:00 +0530

    added to master

---
+++ EmpWageComputation.java
@@ -0,0 +1,7 @@
+import java.util.*;
+//start
+class EmpWageComputationUc1{
+   public static void main(String[] args)throws Exception{
+      System.out.println("Welcome to Employee wage computation program");
+   }
+}
