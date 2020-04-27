74e88d04a3c3a4319ba495fa31146abb59122f5b
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-27 23:36:38 +0530

    added to uc1

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,13 @@
 import java.util.*;
-//start
+//UC-1 Ckeck Attendance
 class EmpWageComputationUc1{
    public static void main(String[] args)throws Exception{
       System.out.println("Welcome to Employee wage computation program");
+      double attCheck=Math.random();
+      if(attCheck>0.5){
+         System.out.println("Employee is present");
+      }else{
+         System.out.println("Employee is absent");
+      }
    }
 }
