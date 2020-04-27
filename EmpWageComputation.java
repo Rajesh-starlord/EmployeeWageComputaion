bb5839e8c7d6aad7d4c22c76bccd4a665b64924a
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-27 23:42:45 +0530

    added uc2

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,18 @@
 import java.util.*;
-//start
-class EmpWageComputationUc1{
+//UC-2 daily wage
+
+class EmpWageComputationUc2{
    public static void main(String[] args)throws Exception{
+      int wage_per_hr=20;
+      int full_day_hr=8;
       System.out.println("Welcome to Employee wage computation program");
+      double attCheck=Math.random();
+      if(attCheck>0.5){
+         System.out.println("Employee is present");
+         System.out.println("daily_wage="+(wage_per_hr*full_day_hr))";
+      }else{
+         System.out.println("Employee is absent");
+         System.out.println("daily_wage="+(wage_per_hr*0))";
+      }
    }
 }
