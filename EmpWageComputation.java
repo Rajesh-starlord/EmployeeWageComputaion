4a2ab7e4c9d39147fc5565496badac0459c9bd22
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-27 23:53:23 +0530

    added uc4

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,25 +1,42 @@
 import java.util.*;
-//UC-3 add part time
+//UC-4 using switch case
 
 class EmpWageComputationUc3{
    public static void main(String[] args)throws Exception{
       int wage_per_hr=20;
       int full_day_hr=8;
       int half_day_hr=4;
+      String emp_status="";
+      String emp_type="";
       System.out.println("Welcome to Employee wage computation program");
       double attCheck=Math.random();
       if(attCheck>0.5){
-         double category=Math.random();
-         if (category>0.5){
-            System.out.println("Employee is present and is a full time");
-            System.out.println("daily_wage="+(wage_per_hr*full_day_hr));
-         }else{
-            System.out.println("Employee is present and is a part time");
-            System.out.println("daily_wage="+(wage_per_hr*half_day_hr));
-         }
+         emp_status="present";
       }else{
-         System.out.println("Employee is absent");
-         System.out.println("daily_wage="+(wage_per_hr*0));
+         emp_status="absent";
       }
-   }
-}
+      switch(emp_status){
+         case "present"://check emp is part time or full time
+               double category=Math.random();
+               if (category>0.5){
+                  emp_type="full_time";
+               }else{
+                  emp_type="part_time";
+               }
+               switch(emp_type){
+                  case "full_time":
+                     System.out.println("Employee is present and is a full time");
+                     System.out.println("daily_wage="+(wage_per_hr*full_day_hr));
+                     break;
+                  case "part_time":
+                     System.out.println("Employee is present and is a part time");
+                     System.out.println("daily_wage="+(wage_per_hr*half_day_hr));
+                     break;
+               }
+         case "absent":
+            System.out.println("Employee is absent");
+            System.out.println("daily_wage="+(wage_per_hr*0));
+         break;
+      }//switch
+   }//main
+}//class
